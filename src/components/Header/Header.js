a5592f7ef442df5717b93883a8664ea922c239bd
FOAna <kristendamon@yandex.ru>
2021-09-24 01:06:08 +0500

    create header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,67 +6,60 @@ import burgerMenuIcon from "../../images/burger.svg";
 
 export default function Header() {
   return (
-    <div className="Header">
-      <header className="Header__content">
-        <div className="Header__leftPart">
-          <img
-            className="Header__logo"
-            src={logo}
-            alt="Логотипчик"
-            height="64"
-          />
-          <ul className="Header__menuList">
-            <li className="Header__menuItem">
-              <a className="Header__menuItemLink" href="/">
-                Популярное
-              </a>
-            </li>
-            <li className="Header__menuItem">
-              <a className="Header__menuItemLink" href="/">
-                Фильмы
-              </a>
-            </li>
-            <li className="Header__menuItem">
-              <a className="Header__menuItemLink" href="/">
-                Сериалы
-              </a>
-            </li>
-            <li className="Header__menuItem">
-              <a className="Header__menuItemLink" href="/">
-                Мультфильмы
-              </a>
-            </li>
-            <li className="Header__menuItem">
-              <a className="Header__menuItemLink" href="/">
-                Избранное
-              </a>
-            </li>
-            <li className="Header__menuItem">
-              <a className="Header__menuItemLink" href="/">
-                Анонсы
-              </a>
-            </li>
-          </ul>
-        </div>
+    <header className="Header">
+      <div className="Header__leftPart">
+        <img className="Header__logo" src={logo} alt="Логотипчик" height="48" />
+        <ul className="Header__menuList">
+          <li className="Header__menuItem">
+            <a className="Header__menuItemLink" href="/">
+              Популярное
+            </a>
+          </li>
+          <li className="Header__menuItem">
+            <a className="Header__menuItemLink" href="/">
+              Фильмы
+            </a>
+          </li>
+          <li className="Header__menuItem">
+            <a className="Header__menuItemLink" href="/">
+              Сериалы
+            </a>
+          </li>
+          <li className="Header__menuItem">
+            <a className="Header__menuItemLink" href="/">
+              Мультфильмы
+            </a>
+          </li>
+          <li className="Header__menuItem">
+            <a className="Header__menuItemLink" href="/">
+              Избранное
+            </a>
+          </li>
+          <li className="Header__menuItem">
+            <a className="Header__menuItemLink" href="/">
+              Анонсы
+            </a>
+          </li>
+        </ul>
+      </div>
 
-        <div className="Header__menuControlls">
-          <a className="Header__userAccount" href="/">
-            <span className="Header__userName">Анастасия</span>
-            <img
-              className="Header__userIcon"
-              src={userIcon}
-              alt="Настюша"
-              width="64px"
-            />
-          </a>
-          <button className="Header__search">
-            <img src={searchIcon} alt="Поиск" width="32" />
-          </button>
-          <button className="Header__burgerMenu">
-            <img src={burgerMenuIcon} alt="Менюшка" height="32" />
-          </button>
-        </div>
-      </header>
-    </div>
+      <div className="Header__menuControlls">
+        <a className="Header__userAccount" href="/">
+          <span className="Header__userName">Анастасия</span>
+          <img
+            className="Header__userIcon"
+            src={userIcon}
+            alt="Настюша"
+            width="48px"
+          />
+        </a>
+        <button className="Header__search">
+          <img src={searchIcon} alt="Поиск" width="24" />
+        </button>
+        <button className="Header__burgerMenu">
+          <img src={burgerMenuIcon} alt="Менюшка" height="16" />
+        </button>
+      </div>
+    </header>
   );
 }
